dd9ce0a7b864d8d38a003ef9d499d5a68578cae4
Martin Schmidt <martin@scoding.dk>
2020-05-19 11:18:55 +0000

    Updated to used client_id

--- src/identity/controllers.py
+++ src/identity/controllers.py
@@ -216,7 +216,7 @@ def consent():
         return redirect(res.redirect_to)
 
     # Is a trusted client.
-    if TRUSTED_CLIENTS and consent_request.client in TRUSTED_CLIENTS.split(';'):
+    if TRUSTED_CLIENTS and consent_request.client.client_id in TRUSTED_CLIENTS.split(';'):
         res = _grant_consent(challenge, consent_request, user, True)
         return redirect(res.redirect_to)
 
