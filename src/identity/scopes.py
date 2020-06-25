fbe348388a9d4479e8cbfd5f19ddb00ac9d0c33b
Martin Schmidt <martin@scoding.dk>
2020-06-25 10:59:36 +0000

    Updated email scope description

--- src/identity/scopes.py
+++ src/identity/scopes.py
@@ -3,7 +3,7 @@ SCOPES = {
     'openid': 'Use the Eloprindelse to verify the user\'s identity.',
     'offline': 'Act on behalf of the user while the user is offline.',
     'profile': 'Access to the users profile information.',
-    'email': 'Access to the users email.',
+    'email': 'Access to the users email address.',
     'meteringpoints.read': 'Read access to information about the users metering points.',
     'measurements.read': 'Read access to measurements from the users metering points.',
     'ggo.read': 'Read access to the users GGOs.',
